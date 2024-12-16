a59e7ae2d68762f24234e35e34fdb3fe5e08a198
safaabuzaid <s.o.abuzaid@gmail.com>
2024-12-16 18:21:04 +0300

    solve exercise of test 1

--- 3_documenting_and_testing/exercises/mystery_1.py
+++ 3_documenting_and_testing/exercises/mystery_1.py
@@ -1,2 +1,21 @@
+
+"""
+This function ading two numbers and return the results.
+
+Parameters:
+Two numbers (int, float)
+
+Return:
+the summetion of the two inputs
+
+Examples:
+>>> mystery_1(2,3)
+5
+
+Author: ....
+Date: 15 December 2024
+
+"""
+
 def mystery_1(a,b):
     return a + b
--- 3_documenting_and_testing/exercises/tests/test_mystery_1.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_1.py
@@ -8,3 +8,18 @@ class TestMystery1(unittest.TestCase):
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_1(0, 0), 0)
+    
+    def test_first(self):
+        """"""
+        self.assertEqual(mystery_1(5, 1), 6)
+ 
+    def test_second(self):
+        """"""
+        self.assertEqual(mystery_1(-3,-6),-9)
+
+    def test_type(self):
+        """"""
+        self.assertEqual(mystery_1(4.1,5.3),9.4)
+
+    #def test_more(self):
+    #    self. assertEqual(mystery_1(3,5,4),13)
