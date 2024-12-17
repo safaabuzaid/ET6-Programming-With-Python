9308990d2ccad9faa7b3c9d1e853b5002a7d75ce
safaabuzaid <s.o.abuzaid@gmail.com>
2024-12-17 21:26:29 +0300

    solve mystery 2

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,3 +1,21 @@
+"""
+This code give the length of the input
+
+parameters:
+List of elements, text
+
+Return:
+The length of the input
+
+Examples:
+>>> mystery_2("Safaa")
+5
+
+>>> mystery_2([1,2,3,4])
+4
+"""
+
+
 def mystery_2(a):
     if len(a) == 0:
         return None
--- 3_documenting_and_testing/exercises/tests/test_mystery_2.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_2.py
@@ -12,3 +12,12 @@ class TestMystery2(unittest.TestCase):
     def test_minimal_string_input(self):
         """"""
         self.assertEqual(mystery_2(''), None)
+
+    def test_numbers(self):
+        self.assertEqual(mystery_2([5,4,3]),3)
+
+    def test_negatives(self):
+        self.assertEqual(mystery_2([-4,-3,-7]),3)
+
+    def test_characters(self):
+        self.assertEqual(mystery_2('Safaa'),5)
