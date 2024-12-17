fcc8c43e74483f6177a937ba3f38f6ab5dd504e2
safaabuzaid <s.o.abuzaid@gmail.com>
2024-12-18 01:58:16 +0300

    mystery_3

--- 3_documenting_and_testing/exercises/mystery_3.py
+++ 3_documenting_and_testing/exercises/mystery_3.py
@@ -1,7 +1,27 @@
+"""
+The module return the smaller number, if the inputs are equal it return the sum of them.
+
+Parameters:
+two integers
+
+Return:
+the smaller number, f(equals)>summation of them
+
+Examples:
+>>> mystery_3(4,1)
+1
+
+>>> mystery_3(4,7)
+4
+
+>>> mystery_3(4,4)
+8
+"""
+
 def mystery_3(a, b):
     if a < b:
         return a
     elif a > b:
         return b
-    else:
+    else: 
         return a + b
--- 3_documenting_and_testing/exercises/tests/test_mystery_3.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_3.py
@@ -8,3 +8,15 @@ class TestMystery3(unittest.TestCase):
     def test_minimal_input(self):
         """"""
         self.assertEqual(mystery_3(0, 0), 0)
+    
+    def test_1(self):
+        self.assertEqual(mystery_3(3,6),3)
+
+    def test_2(self):
+        self.assertEqual(mystery_3(6,7),6)
+
+    def test_3(self):
+        self.assertEqual(mystery_3(3,3),6)
+    
+    def test_4(self):
+        self.assertEqual(mystery_3(7,6),6)
